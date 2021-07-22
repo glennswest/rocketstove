d7446dcb9b48c93a4b8cb099674f1470d51a4639
gwest <gwest@gwest@redhat.com-mac>
2021-07-22 12:52:52 -0500

    Regen towera p01b and p02a

---
+++ run.sh
@@ -0,0 +1,12 @@
+export oscad=/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD
+
+$oscad -q -o towera-bpanel.stl towera-bpanel.scad
+$oscad -q -o towera-fpanel.stl towera-fpanel.scad
+$oscad -q -o towera-p01a.stl   towera-p01a.scad
+$oscad -q -o towera-p01b.stl   towera-p01b.scad
+$oscad -q -o towera-p02a.stl   towera-p02a.scad
+$oscad -q -o towera-p02b.stl   towera-p02b.scad
+$oscad -q -o towerb-p01a.stl   towerb-p01a.scad
+$oscad -q -o towerb-p01b.stl   towerb-p01b.scad
+$oscad -q -o towerb-p02a.stl   towerb-p02a.scad
+$oscad -q -o towerb-p02b.stl   towerb-p02b.scad
