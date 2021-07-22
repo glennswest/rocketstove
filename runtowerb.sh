b3f1c1c818cbe52324bfa8c2c61f91702328506d
gwest <gwest@gwest@redhat.com-mac>
2021-07-22 13:30:30 -0500

    Try to fix with ideamaker

---
+++ runtowerb.sh
@@ -0,0 +1,6 @@
+export oscad=/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD
+
+$oscad -q -o towerb-p01a.stl   towerb-p01a.scad
+$oscad -q -o towerb-p01b.stl   towerb-p01b.scad
+$oscad -q -o towerb-p02a.stl   towerb-p02a.scad
+$oscad -q -o towerb-p02b.stl   towerb-p02b.scad
